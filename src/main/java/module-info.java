46b9bee128de6be05c28210ea50c8268786904aa
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2025-04-04 13:37:24 +0200

    First commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module simulacion.simulacion_zombies {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens simulacion.simulacion_zombies to javafx.fxml;
+    exports simulacion.simulacion_zombies;
+}
