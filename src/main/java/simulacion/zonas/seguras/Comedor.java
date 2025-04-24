33e70d60f04b48a86dbf130921dd09059e26d27e
jaizu23 <157589155+jaizu23@users.noreply.github.com>
2025-04-24 18:15:04 +0200

    Added LabelUpdateConcurrentHashMapArray and updated view labels

--- src/main/java/simulacion/zonas/seguras/Comedor.java
+++ src/main/java/simulacion/zonas/seguras/Comedor.java
@@ -1,4 +1,22 @@
 package simulacion.zonas.seguras;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import simulacion.estructuras_de_datos.LabelUpdateConcurrentHashMap;
+import simulacion.seres.Humano;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Comedor extends Refugio{
+    private final LabelUpdateConcurrentHashMap<Humano> humanosComedor = new LabelUpdateConcurrentHashMap<>(10000);
+
+    private final IntegerProperty contadorComida = new SimpleIntegerProperty(0);
+
+    public LabelUpdateConcurrentHashMap<Humano> getHumanosComedor() {
+        return humanosComedor;
+    }
+
+    public IntegerProperty getContadorComida() {
+        return contadorComida;
+    }
 }
